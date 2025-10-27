aadd73fcff32a4d0d216e1a2606556246015c3e0
Md. Jubayer Shikder <validjubayer@gmail.com>
2025-10-27 08:45:20 +0600

    fix(ui):  fix marquee responsive size

--- app/sections/LogoMarquee.tsx
+++ app/sections/LogoMarquee.tsx
@@ -7,16 +7,17 @@ const LogoMarquee = () => {
     (_, i) => `/marquee/logo${i + 1}.png`
   );
   return (
-    <Marquee >
+    <Marquee>
       {logos.map((src, i) => (
-        <Image
-          key={i}
-          src={src}
-          alt={`logo ${i + 1}`}
-          width={200}
-          height={200}
-          className="mx-10"
-        />
+        <div key={i} className="w-35 lg:w-50 mx-5 lg:mx-10 mt-20 lg:mt-0">
+          <Image
+            key={i}
+            src={src}
+            alt={`logo ${i + 1}`}
+            width={200}
+            height={200}
+          />
+        </div>
       ))}
     </Marquee>
   );
